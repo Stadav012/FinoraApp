19fd6909c05ae6cd9d48944c2ba0d6aaa2530cc7
zviko163 <shammah.dzwairo@ashesi.edu.gh>
2026-06-11 22:30:40 +0200

    testing different gemini model

--- api/src/controllers/transactionController.js
+++ api/src/controllers/transactionController.js
@@ -29,7 +29,7 @@ const processTransaction = async (req, res) => {
     `;
 
     // calling the LLM for extraction
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
     const result = await model.generateContent(prompt);
     let responseText = result.response.text();
     
